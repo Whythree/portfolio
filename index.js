482b82d3f3a404b54e584480e97eb800ae403657
Whythree <whythree1.dollars@gmail.com>
2025-12-18 11:05:37 +0100

    Changes to Text and minor additions to make presentation more general and interesting to more employers

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 import GlitchedWriter from 'https://cdn.skypack.dev/glitched-writer'
 import {presets} from "https://cdn.skypack.dev/glitched-writer";
 
-const phrases = ['Designer', 'Coder']
+const phrases = ['Business', 'Code', 'Design']
 
 const writer = new GlitchedWriter("#glitched-text", {...presets.neo}
 );
